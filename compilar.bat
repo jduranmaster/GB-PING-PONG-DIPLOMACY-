6e94970b72793a8beb9bf7a2eeda90162e70c00c
RYOGA (a.k.a. jduranmaster) <ryogasingps@gmail.com>
2015-09-06 20:21:55 +0200

    changes in the compitalion script

--- compilar.bat
+++ compilar.bat
@@ -1,5 +1,5 @@
 lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o PingPongDiplomacy.o PingPongDiplomacy.c
-lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt1 -Wl-yo4 -Wl-ya0 -o PingPongDiplomacy-simpleBankingMBC1-20141214.gb PingPongDiplomacy.o
+lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt1 -Wl-yo4 -Wl-ya0 -o PingPongDiplomacy-rom.gb PingPongDiplomacy.o
 
 del *.o *.lst *.map *.sym
 
